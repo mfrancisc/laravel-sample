988967c1032768fe01ae88b070fe651424c2966b
Francisc Munteanu <munteanu.francisc@gmail.com>
2015-12-11 00:24:09 +0100

    Manage Your Assests

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,16 +7,27 @@
   <title>Document</title>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" title="" type="" />
  <link rel="stylesheet" href="{{ elixir('css/all.css')}}" title="" type="" /> 
-  
+
 </head>
 
 <body>
 <div class="container">
-  
+
+  @include('partials.flash')
+
   @yield('content')
 
 </div>
 
+
+<script src="//code.jquery.com/jquery.js" ></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" ></script>
+
+<script type="text/javascript" charset="utf-8">
+
+ $('div.alert').not('.alert-important').delay(3000).slideUp(300); 
+
+</script>
 
 @yield('footer')
 
